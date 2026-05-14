4829480a51aaeb3a3391c85a87b6ad410fe8a766
Alexander <pchp250g8@tutamail.com>
2026-05-14 15:16:20 +0300

    Add project files.

---
+++ HelloOpenGL/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GlControl1 = New OpenTK.GLControl()
+        Me.SuspendLayout()
+        '
+        'GlControl1
+        '
+        Me.GlControl1.BackColor = System.Drawing.Color.Black
+        Me.GlControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GlControl1.Location = New System.Drawing.Point(0, 0)
+        Me.GlControl1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GlControl1.Name = "GlControl1"
+        Me.GlControl1.Size = New System.Drawing.Size(800, 450)
+        Me.GlControl1.TabIndex = 0
+        Me.GlControl1.VSync = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GlControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GlControl1 As OpenTK.GLControl
+End Class
---
+++ HelloOpenGL/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports OpenTK
+Imports OpenTK.Graphics
+Public Class Form1
+    Private nRedByte As Byte
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        nRedByte = 0
+        AddHandler Application.Idle, AddressOf Me.Application_Idle
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        nRedByte += 1
+        GlControl1.Invalidate()
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        RemoveHandler Application.Idle, AddressOf Me.Application_Idle
+    End Sub
+
+    Private Sub GlControl1_Paint(sender As Object, e As PaintEventArgs) Handles GlControl1.Paint
+        Dim glColor = Color.FromArgb(nRedByte, 0, 0)
+        GL.ClearColor(glColor)
+        GL.Clear(ClearBufferMask.ColorBufferBit Or ClearBufferMask.StencilBufferBit)
+        GL.ClearDepth(0)
+        GlControl1.SwapBuffers()
+    End Sub
+    Private Sub Application_Idle(sender As Object, e As EventArgs)
+        nRedByte += 1
+        Invalidate()
+    End Sub
+
+End Class
